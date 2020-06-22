5b2e4bc405949bbb636b04a8968185ecc8292b0a
Abhilasha kumari <abhilashakumari455@gmail.com>
2020-06-22 13:44:12 +0530

    Updates

---
+++ precache-manifest.518c5e30c60e11d61b05ca06ebb6fdb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19026f10a5f50eeaeb2a52ce5ed84687",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "4aea98157f894e16ed2d",
+    "url": "/my-project/static/css/main.51c871c1.chunk.css"
+  },
+  {
+    "revision": "fca2ea60a1bcaf40cf7a",
+    "url": "/my-project/static/js/2.73c95f51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-project/static/js/2.73c95f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aea98157f894e16ed2d",
+    "url": "/my-project/static/js/main.35c00064.chunk.js"
+  },
+  {
+    "revision": "4d80c05a32161a7b1e73",
+    "url": "/my-project/static/js/runtime-main.28770c3e.js"
+  },
+  {
+    "revision": "02bd74010c40f758ebacaa5fc6b16bbe",
+    "url": "/my-project/static/media/logo.02bd7401.jpg"
+  }
+]);
